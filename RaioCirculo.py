d11972ff19a1c61d01e54b5af31e633315de9205
UmCaraBom <102264046+UmCaraBom@users.noreply.github.com>
2022-09-08 12:20:58 -0300

    Add files via upload

---
+++ RaioCirculo.py
@@ -0,0 +1,7 @@
+#criar entrada raio de um círculo e calcular perímetro e área
+raio = float(input(">> Digite o raio do círculo: "))
+perimetro = 2 * 3.14 * raio
+area = 3.14 * raio ** 2
+print("> O perímetro do círculo é: ", perimetro)
+print("> A área do círculo é: ", area)
+
